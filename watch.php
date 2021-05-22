3d5e0651565afc6389b7541a7829782d161e72b0
AndrewXVII <80914289+AndrewXVII@users.noreply.github.com>
2021-05-22 14:50:30 +0300

    Add files via upload

---
+++ watch.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta charset="utf-8">
+<link rel="stylesheet" type = "text/css" href="assets/styles.css">
+
+<link rel="shortcut icon" href="ikon.ico" type="image/x-icon" />
+</head>
+</html>
+<?php
+
+
+require_once("includes/classes/connection.php");
+require_once("includes/classes/vidPlayer.php");
+require_once("includes/classes/video.php");
+
+if(!isset($_GET['id'])){
+    echo "no id set";
+  
+}
+
+?>
+
+<?php
+$movie = new Video($dbcon , $_GET['id'] );
+
+
+
+$vidPlayer = new vidPlayer($movie);
+
+echo $vidPlayer->stream(true);
+?>
+
+
+
