9773f4c0f649a14562d706e85c22fc1ed57e0f89
AndrewXVII <80914289+AndrewXVII@users.noreply.github.com>
2021-06-16 16:42:19 +0300

    Update connection.php

--- includes/classes/connection.php
+++ includes/classes/connection.php
@@ -1,6 +1,5 @@
 <?php
-ob_start(); 
-session_start();
+
 
 
 $dsn = 'mysql:dbname=moviestream;host=localhost';
@@ -8,12 +7,19 @@ $dbuser = 'root';
 $dbpass = '';
 
 
-try{
+try{ //try to connect to DB
     
-$dbcon= NEW PDO($dsn,$dbuser,$dbpass);
-$dbcon->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION); //SET ATTRIBUTE ERRMODE TO ERRMODE EXCEPTION
+$dbcon= NEW PDO($dsn,$dbuser,$dbpass); // PDO = PHP DATA OBJECT
+
+//ACCESSING THE PROPERTY OF PDO CALLED ATTR_ERRMODE;ERRMODE_EXCEPTION
+$dbcon->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);     //Sets an attribute on the database handle.
+//PDO::ERRMODE_EXCEPTION: Throw exceptions.
+//PDO::ATTR_ERRMODE: Error reporting.
+
+
 }
-catch(PDOException $e)
+//if connection failed:
+catch(PDOException $e )  // listening of variable of type PDOException called $e
 {
 
 die("Connection failed".$e->getMessage());
