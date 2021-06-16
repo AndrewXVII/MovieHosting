d0585e1bb0428e44b6bf75f8236efb840fb7c874
AndrewXVII <80914289+AndrewXVII@users.noreply.github.com>
2021-06-16 16:41:32 +0300

    Update video.php

--- includes/classes/video.php
+++ includes/classes/video.php
@@ -4,31 +4,31 @@ class Video{
 
     protected $dbcon ; 
     protected $sqldata;
-    
 
-
-    public function __construct($dbcon ,$input){
+    public function __construct($dbcon ,$id){
         $this->dbcon = $dbcon;
 
-        if(is_array($input)){
-            $this->sqldata = $input;
-
-        }
-
-        else{
-            $query = $this->dbcon->prepare("SELECT * FROM videos WHERE id=:id");
-            $query->bindValue(":id" ,  $input);
+        
+            $query = $this->dbcon->prepare("SELECT * FROM videos WHERE id=:id");  //Prepares an SQL statement for execution
+            $query->bindValue(":id" ,  $id);
             $query->execute();
 
-            $this->sqldata = $query->fetch(PDO::FETCH_ASSOC);
+            $this->sqldata = $query->fetch(PDO::FETCH_ASSOC); //fetch the result so we can see it
+               // $this keyword is used to access non-static members of a class(variables or functions) for the current object.
+        
+
+            //ASSOCIATIVE ARRAY IS AN ARRAY THAT HAVE A KEY AND VALUE
+            //Returns an associative array that corresponds to the fetched row or null if there are no more rows. 
+            //Return next row as an array indexed by column name
+            // Fetches the next row from a result set
         }
 
 
-    }
+    
 
    
 public function getId (){
-   return $this->sqldata['id'];
+   return $this->sqldata['id']; //return sqldata ID
 }
 public function getTitle (){
     return $this->sqldata['title'];
